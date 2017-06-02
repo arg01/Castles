d383f6dc152410d882f25421edc5b5b9f3cf6caf
Matt <matt@robinsonm.com>
2017-06-02 22:42:14 +0100

    ahh wsgi

--- castles/settings.py
+++ castles/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'saticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
--- castles/wsgi.py
+++ castles/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "castles.settings")
-
+application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
