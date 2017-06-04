f4d067557c9407b68241d300a455a3e455ae1eca
Matt <matt@robinsonm.com>
2017-06-04 14:32:08 +0100

    general cleaning

---
+++ game/gameScript.py
@@ -0,0 +1,89 @@
+from .models import *
+
+
+def fight(player, opponent):
+    pScore = 0
+    oScore = 0
+    i=0
+    value=0
+    while i<10:
+        army=player[i]
+        opponentArmy=opponent[i]
+        value+=1
+        if army > opponentArmy:
+            pScore+=value
+        elif army < opponentArmy:
+            oScore+=value
+        else:
+            pScore+=(value/2)
+            oScore+=(value/2)
+        i+=1
+
+    if pScore > oScore:
+        return 'Win'
+    elif pScore < oScore:
+        return 'Lose'
+    else:
+        return 'Tie'
+
+def playRound():
+    playerNo = players.objects.filter(played=False).count()
+    allocation = []
+    temp =0
+    tempAllocation =[]
+    i=0
+    tempKey=None
+    keys=[]
+    tempPlayKeys=None
+    playKeys=[]
+    while i<playerNo:
+        n=1
+        tempAllocation=[]
+        while n<=10:
+            varAll = 'Allocations' + str(n)
+            temp = players.objects.values_list(varAll, flat=True).filter(played=False)
+            tempAllocation.append(temp[i])
+            n+=1
+        tempName = players.objects.values_list('pk', flat=True).filter(played=False)
+        keys.append(tempName[i])
+        playKeys = players.objects.values_list('pk', flat=True).filter(played=True)
+        allocation.append(tempAllocation)
+        i+=1
+
+
+    loop=0
+
+    while loop<playerNo:
+        n=0
+        while n<playerNo:
+            if n!=loop:
+                result = fight(allocation[loop], allocation[n])
+                if result == 'Win':
+                    player = players.objects.get(pk=keys[loop])
+                    player.wins+=1
+                    player.save()
+                elif result == 'Lose':
+                    player = players.objects.get(pk=keys[loop])
+                    player.losses+=1
+                    player.save()
+                else:
+                    player = players.objects.get(pk=keys[loop])
+                    player.ties+=1
+                    player.save()
+            n+=1
+        loop+=1
+    playLoop=0
+    while playLoop<playerNo:
+        player = players.objects.get(pk=keys[playLoop])
+        player.played=True
+        player.save()
+        playLoop+=1
+    plays=len(playKeys)
+    print(playKeys)
+    playsLoop=0
+    while playsLoop<plays:
+        player = players.objects.get(pk=playKeys[playsLoop])
+
+        player.display=True
+        player.save()
+        playsLoop+=1
--- game/views.py
+++ game/views.py
@@ -5,7 +5,7 @@ from .forms import playersForm
 from django.http import HttpResponseRedirect
 import django_tables2 as tables
 import logging
-
+from .gameScript import *
 logger = logging.getLogger()
 
 def index(request):
@@ -39,93 +39,8 @@ def play(request):
         form = playersForm()
     return render(request, 'game/play.html', {'form':form})
 
-def fight(player, opponent):
-    pScore = 0
-    oScore = 0
-    i=0
-    value=0
-    while i<10:
-        army=player[i]
-        opponentArmy=opponent[i]
-        value+=1
-        if army > opponentArmy:
-            pScore+=value
-        elif army < opponentArmy:
-            oScore+=value
-        else:
-            pScore+=(value/2)
-            oScore+=(value/2)
-        i+=1
-
-    if pScore > oScore:
-        return 'Win'
-    elif pScore < oScore:
-        return 'Lose'
-    else:
-        return 'Tie'
-
-
 
 
 def run_script(request):
-    playerNo = players.objects.filter(played=False).count()
-    allocation = []
-    temp =0
-    tempAllocation =[]
-    i=0
-    tempKey=None
-    keys=[]
-    tempPlayKeys=None
-    playKeys=[]
-    while i<playerNo:
-        n=1
-        tempAllocation=[]
-        while n<=10:
-            varAll = 'Allocations' + str(n)
-            temp = players.objects.values_list(varAll, flat=True).filter(played=False)
-            tempAllocation.append(temp[i])
-            n+=1
-        tempName = players.objects.values_list('pk', flat=True).filter(played=False)
-        keys.append(tempName[i])
-        playKeys = players.objects.values_list('pk', flat=True).filter(played=True)
-        allocation.append(tempAllocation)
-        i+=1
-
-
-    loop=0
-
-    while loop<playerNo:
-        n=0
-        while n<playerNo:
-            if n!=loop:
-                result = fight(allocation[loop], allocation[n])
-                if result == 'Win':
-                    player = players.objects.get(pk=keys[loop])
-                    player.wins+=1
-                    player.save()
-                elif result == 'Lose':
-                    player = players.objects.get(pk=keys[loop])
-                    player.losses+=1
-                    player.save()
-                else:
-                    player = players.objects.get(pk=keys[loop])
-                    player.ties+=1
-                    player.save()
-            n+=1
-        loop+=1
-    playLoop=0
-    while playLoop<playerNo:
-        player = players.objects.get(pk=keys[playLoop])
-        player.played=True
-        player.save()
-        playLoop+=1
-    plays=len(playKeys)
-    print(playKeys)
-    playsLoop=0
-    while playsLoop<plays:
-        player = players.objects.get(pk=playKeys[playsLoop])
-
-        player.display=True
-        player.save()
-        playsLoop+=1
+    playRound()
     return HttpResponseRedirect('/game/results')
